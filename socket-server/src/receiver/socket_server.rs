f2009bc09b23339b0a2b4102cfedcdb4f3321cb6
luiggy2620 <luiggymamani@gmail.com>
2024-06-13 15:55:38 -0400

    ci: screen socket server receiver implemented

---
+++ socket-server/src/receiver/socket_server.rs
@@ -0,0 +1,33 @@
+use serde_json;
+use std::io::{BufRead, BufReader};
+use std::os::unix::net::UnixListener;
+
+use crate::models::events::window::MyWindowEvent;
+
+pub struct SocketServer {
+    listener: UnixListener,
+}
+
+impl SocketServer {
+    pub fn new(socket_path: &str) -> std::io::Result<Self> {
+        let listener = UnixListener::bind(socket_path)?;
+        Ok(SocketServer { listener })
+    }
+
+    pub fn start(&self) -> std::io::Result<()> {
+        match self.listener.accept() {
+            Ok((socket, _)) => {
+                let reader = BufReader::new(&socket);
+                for line in reader.lines() {
+                    let buf = line?;
+                    let event: MyWindowEvent = serde_json::from_str(&buf).unwrap();
+                    println!("{:?}", event);
+                }
+            }
+            Err(error) => {
+                println!("error listening to: {:?}", error);
+            }
+        }
+        Ok(())
+    }
+}
