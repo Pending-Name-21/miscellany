d84ff6e1dff002da65490c325f5bf1e22996e07b
luiggy2620 <luiggymamani@gmail.com>
2024-06-14 18:03:50 -0400

    test: unit tests added

--- socket-server/src/receiver/socket_server.rs
+++ socket-server/src/receiver/socket_server.rs
@@ -4,12 +4,14 @@ use std::io::{BufRead, BufReader, Error, ErrorKind, Result};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
 
-use crate::models::events::window::MyWindowEvent;
-
 use super::receiver_handler::handle_received_event;
+use crate::models::events::window::MyWindowEvent;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 pub struct SocketServer {
     listener: UnixListener,
+    running: Arc<AtomicBool>,
 }
 
 impl SocketServer {
@@ -27,11 +29,16 @@ impl SocketServer {
         }
 
         let listener = UnixListener::bind(socket_path)?;
-        Ok(SocketServer { listener })
+        let running = Arc::new(AtomicBool::new(true));
+        Ok(SocketServer { listener, running })
+    }
+
+    pub fn stop(&self) {
+        self.running.store(false, Ordering::SeqCst);
     }
 
     pub fn start(&self) -> std::io::Result<()> {
-        loop {
+        while self.running.load(Ordering::SeqCst) {
             match self.listener.accept() {
                 Ok((socket, _)) => {
                     let reader = BufReader::new(&socket);
@@ -46,5 +53,37 @@ impl SocketServer {
                 }
             }
         }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::events::key::MyKey;
+    use std::io::Write;
+    use std::thread;
+    use tempfile::TempDir;
+
+    #[test]
+    fn test_socket_server() {
+        let temp_dir = TempDir::new().unwrap();
+        let socket_path = temp_dir.path().join("test.sock");
+        let server = SocketServer::new(socket_path.to_str().unwrap()).unwrap();
+        let server_ref = Arc::new(server);
+        let server_test = server_ref.clone();
+
+        let handle = thread::spawn(move || {
+            server_test.start().unwrap();
+        });
+
+        let mut client = UnixStream::connect(socket_path).unwrap();
+        let test_event = MyWindowEvent::MyKeyPressed(MyKey::MyA);
+        let test_event_json = serde_json::to_string(&test_event).unwrap();
+
+        writeln!(client, "{}", test_event_json).unwrap();
+
+        server_ref.stop();
+        handle.join().unwrap();
     }
 }
