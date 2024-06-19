3a9952d033fc998748a1b4f113111c4772d88b21
victor-villca <villcavictorleon@gmail.com>
2024-06-19 18:18:59 -0400

    feat: animations added to the script

--- coffe-time-scripts/game-console-execution/execute.sh
+++ coffe-time-scripts/game-console-execution/execute.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# function to show the loading animation
+show_loading() {
+    # loading animation
+    for i in {1..100}; do
+        tput sc
+        echo -n "Loading... [$(tput setaf 1)$i%$(tput sgr0)]"
+        sleep 0.009
+        tput rc
+    done
+    printf "\n"
+}
+
 #function to init a background process for a given script
 start_process() {
     local cmd=$1
+    local cmd=$2
+    echo "Initializing $msg ..."
     #execute command and unpair from terminal
-    $cmd >&1 & disown
+    ($cmd >&1 & disown)
+    show_loading
 }
 
 # init socket-server
@@ -17,7 +32,7 @@ echo "screen initialized..."
 
 #init game with library
 java -Djava.library.path=console/ -jar game.jar
-echo "game initilazed..."
+show_loading
 
 # success message
 echo "All processes successfully initialized. :)"
